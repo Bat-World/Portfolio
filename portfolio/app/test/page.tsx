79818a324d3d1565bb09acdc27662c6223757d9a
Second <drewuncle863724lu@gmail.com>
2025-07-29 13:42:26 +0800

    all things prepared

--- portfolio/app/test/page.tsx
+++ portfolio/app/test/page.tsx
@@ -1,128 +1,64 @@
 "use client";
 
 import { useState } from "react";
-import { useEffect } from "react";
-import UserList from "../../components/UserList";
 
-type Task = {
-  id: string;
-  name: string;
-  completed: boolean;
-};
+const Test = () => {
+  const [filter, setFilter] = useState<
+    "all" | "clothing" | "food" | "electronics"
+  >("all");
 
-type User = {
-  id: number;
-  name: string;
-  email: string;
-};
-const ToDo = () => {
-  const [tasks, setTasks] = useState<Task[]>([]);
-  const [input, setInput] = useState("");
-  const [filter, setFilter] = useState<"all" | "active" | "completed">("all");
-  const [users, setUsers] = useState<User[]>([]);
+  const initialProducts = [
+    { id: 1, name: "T-Shirt", category: "Clothing", price: 20 },
+    { id: 2, name: "Headphones", category: "Electronics", price: 100 },
+    { id: 3, name: "Apple", category: "Food", price: 2 },
+    { id: 4, name: "Jeans", category: "Clothing", price: 40 },
+    { id: 5, name: "TV", category: "Electronics", price: 500 },
+  ];
 
-  const handleAddTask = () => {
-    const newTask = {
-      id: Date.now().toString(),
-      name: input,
-      completed: false,
-    };
-    setTasks((prev) => [...prev, newTask]);
-    setInput("");
-  };
-
-  const handleCompleteTask = (id: string) => {
-    setTasks((prev) =>
-      prev.map((task) =>
-        task.id === id ? { ...task, completed: !task.completed } : task
-      )
-    );
-  };
-
-  const handleDeleteTask = (id: string) => {
-    setTasks((prev) => prev.filter((task) => task.id !== id));
-  };
-
-  const filteredTasks = tasks.filter((task) => {
-    if (filter === "active") return !task.completed;
-    if (filter === "completed") return task.completed;
+  const filteredProducts = initialProducts.filter((product) => {
+    if (filter === "clothing") return product.category === "Clothing";
+    if (filter === "food") return product.category === "Food";
+    if (filter === "electronics") return product.category === "Electronics";
     return true;
   });
 
-  const getUsers = async () => {
-    const data = await fetch("https://jsonplaceholder.typicode.com/users");
-    const res = await data.json();
-    setUsers(res);
-  };
-
-  useEffect(() => {
-    getUsers();
-    const storedTasks = localStorage.getItem("tasks");
-    if (storedTasks) {
-      setTasks(JSON.parse(storedTasks));
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-  }, [tasks]);
-
   return (
-    <div className="">
-      <input
-        type="text"
-        value={input}
-        placeholder="add task"
-        onChange={(e) => setInput(e.target.value)}
-      />
-      <button onClick={handleAddTask} className="pointer-cursor">
-        create task
-      </button>
-      <div>
-        <button onClick={() => setFilter("all")} className="cursor-pointer m-4">
+    <div>
+      <div className="flex flex-row gap-5">
+        <div
+          className={`cursor-pointer bg-gray-500 ${
+            filter === "all" ? "bg-blue-500" : "bg-gray-500"
+          }`}
+          onClick={() => setFilter("all")}
+        >
           All
-        </button>
-        <button
-          onClick={() => setFilter("active")}
-          className="cursor-pointer m-4"
-        >
-          Active
-        </button>
-        <button
-          onClick={() => setFilter("completed")}
-          className="cursor-pointer m-4"
-        >
-          Completed
-        </button>
+        </div>
+        <div className={`cursor-pointer bg-gray-500 ${
+            filter === "clothing" ? "bg-blue-500" : "bg-gray-500"
+          }`}
+          onClick={() => setFilter("clothing")}>Clothing</div>
+        <div className={`cursor-pointer bg-gray-500 ${
+            filter === "electronics" ? "bg-blue-500" : "bg-gray-500"
+          }`}
+          onClick={() => setFilter("electronics")}>Electronics</div>
+        <div className={`cursor-pointer bg-gray-500 ${
+            filter === "food" ? "bg-blue-500" : "bg-gray-500"
+          }`}
+          onClick={() => setFilter("food")}>Food</div>
       </div>
       <ul>
-        {filteredTasks.map((task) => (
-          <li key={task.id}>
-            <input
-              type="checkbox"
-              onChange={() => handleCompleteTask(task.id)}
-            />
-            <span
-              className={`text-white ${task.completed ? "line-through" : ""}`}
-            >
-              {task.name}
-            </span>
-            <button
-              className="text-red-500 cursor-pointer"
-              onClick={() => handleDeleteTask(task.id)}
-            >
-              delete task
-            </button>
+        {filteredProducts.map((p) => (
+          <li key={p.id}>
+            <div className="flex flex-row gap-5">
+              <p>Name: {p.name}</p>
+              <p>Category: {p.category}</p>
+              <p>Price ${p.price}</p>
+            </div>
           </li>
         ))}
       </ul>
-      <span>
-        {tasks.length} total tasks, {tasks.filter((t) => t.completed).length}{" "}
-        completed
-      </span>
-      <UserList users={users} />
     </div>
   );
 };
 
-export default ToDo;
+export default Test;
